17ab14b4c19111dcc3179dd04a80d5c4d2a2235b
brenno-calado <brenno.calado@gmail.com>
2021-05-15 20:21:41 -0300

    add hide-menu.svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,24 +12,8 @@ const Header = () => {
           ${showHeader && 'with-header'}`
         }
         onClick={ () => setHeader(!showHeader) }
-      >
-        <svg width="30" height="30" version="1.1" viewBox="0 0 16.93 16.93" xmlns="http://www.w3.org/2000/svg">
-          <g>
-            <path
-              d="m1.697 0a1.5 1.5 0 0 0-1.5 1.5 1.5 1.5 0 0 0 1.5
-              1.5h13.54a1.5 1.5 0 0 0 1.5-1.5 1.5 1.5 0 0 0-1.5-1.5h-13.54z"
-            />
-            <path
-              d="m1.697 6.967a1.5 1.5 0 0 0-1.5 1.5 1.5 1.5 0 0 0
-              1.5 1.5h13.54a1.5 1.5 0 0 0 1.5-1.5 1.5 1.5 0 0 0-1.5-1.5h-13.54z"
-            />
-            <path
-              d="m1.697 13.93a1.5 1.5 0 0 0-1.5 1.5 1.5 1.5 0 0 0
-              1.5 1.5h13.54a1.5 1.5 0 0 0 1.5-1.5 1.5 1.5 0 0 0-1.5-1.5h-13.54z"
-            />
-          </g>
-        </svg>
-      </button>
+        aria-label="menu button"
+      />
       <header className={ `App-header ${!showHeader && 'hide-header'}` }>
         <Link to="/portfolio">Home</Link>
         <Link to="/portfolio/blog">Blog</Link>
