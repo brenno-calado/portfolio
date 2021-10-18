9561ddae8356c8ec1aeb7bf3d412c5a4d0da41ee
brenno-calado <brenno.calado@gmail.com>
2021-10-18 11:18:16 -0300

    project-item display flex for mobile, dark-card, light-card

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MyContext from '../../context/MyContext';
 import { projects } from '../../utils/variables.json';
 
-const Projects = () => (
-  <>
-    <h1>Projects</h1>
-    <ul className="project-list">
-      {
-        projects.map((project, index) => (
-          <li key={ index }>
-            <img src={ project.src } alt={ project.title } />
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={ project.link }
-            >
-              { project.title }
-            </a>
-            <p>
-              { project.description }
-            </p>
-          </li>
-        ))
-      }
-    </ul>
-  </>
-);
+const Projects = () => {
+  const { darkMode } = useContext(MyContext);
+  const projectItem = `${darkMode ? 'dark-card' : 'light-card'} project-item`;
+  return (
+    <>
+      <h1>Projects</h1>
+      <ul className="project-list">
+        {
+          projects.map((project, index) => (
+            <li key={ index } className={ projectItem }>
+              <img src={ project.src } alt={ project.title } width="100%" />
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={ project.link }
+              >
+                {project.title}
+              </a>
+              <p>
+                {project.description}
+              </p>
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+};
 
 export default Projects;
