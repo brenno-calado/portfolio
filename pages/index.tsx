9f5fa55ab18e17e9bd09a7be8cecf11ba7aeb335
Brenno Calado <brenno.calado@gmail.com>
2024-10-09 22:35:17 -0300

    fix: change to client side fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,52 +1,44 @@
 import {
-  Background,
   Contact,
-  Experience,
   Projects,
-  Skills,
   Welcome,
 } from "@/components";
 import { Inter } from "next/font/google";
 import { PinnedRepos } from "./api/pinned-repos";
-import { GetServerSidePropsContext } from "next";
 import { RepoImage } from "@/models/project.model";
-
-interface IndexProps { pinnedRepos: PinnedRepos[], imageUrls: RepoImage[] }
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export async function getServerSideProps({ res }: GetServerSidePropsContext) {
-  res.setHeader(
-    'Cache-Control',
-    'public, s-maxage=10, stale-while-revalidate=59',
-  );
+export default function Home() {
+  const [imageUrls, setImageUrls] = useState<RepoImage[]>();
+  const [pinnedRepos, setPinnedRepos] = useState<PinnedRepos[]>();
 
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pinned-repos`);
-    const pinnedRepos = (await res.json()) as { data: PinnedRepos[] };
-
-    let imageUrls: RepoImage[] = [];
-    pinnedRepos.data.forEach((repo) => {
-      imageUrls.push({ url: `${repo.url}/blob/main/demo.gif?raw=true`, repo: repo.name })
-    });
-
-    return {
-      props: { imageUrls, pinnedRepos: pinnedRepos.data }
+  useEffect(() => {
+    const fetchRepos = async () => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pinned-repos`);
+      const pinnedRepos = (await res.json()) as { data: PinnedRepos[] };
+  
+      let imageUrls: RepoImage[] = [];
+      pinnedRepos.data.forEach((repo) => {
+        imageUrls.push({ url: `${repo.url}/blob/main/demo.gif?raw=true`, repo: repo.name })
+      });
+  
+      setImageUrls(imageUrls);
+      setPinnedRepos(pinnedRepos.data);
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
+  };
 
-    return {
-      props: {}
-    }
-  }
-}
+  fetchRepos();
+  }, []);
 
-export default function Home({ imageUrls, pinnedRepos }: IndexProps) {
   return (
     <main className={`${inter.className} p-4 max-w-5xl md:mx-auto`}>
       <Welcome />
-      <Projects imageUrls={imageUrls} pinnedRepos={pinnedRepos}  />
+      <Projects imageUrls={imageUrls ?? []} pinnedRepos={pinnedRepos ?? []}  />
       <Contact />
     </main>
   );
