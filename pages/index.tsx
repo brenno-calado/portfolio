c489f04d37be551a4b726503209dc59d20e0f8c0
Brenno Calado <brenno.calado@gmail.com>
2025-01-16 13:20:43 -0300

    fix: fetch absolute url

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   useEffect(() => {
     const fetchRepos = async () => {
     try {
-      const res = await fetch('/api/pinned-repos');
+      const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/pinned-repos`);
       const pinnedRepos = (await res.json()) as { data: PinnedRepos[] };
   
       let imageUrls: RepoImage[] = [];
