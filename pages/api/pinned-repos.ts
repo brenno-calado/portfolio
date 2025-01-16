d7fdffa67bc99f7d885b888bd7e5e0b3831e27a5
Brenno Calado <brenno.calado@gmail.com>
2025-01-16 13:12:09 -0300

    fix: log env

--- pages/api/pinned-repos.ts
+++ pages/api/pinned-repos.ts
@@ -28,6 +28,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
+    console.log('GH_GRAPHQL_ENDPOINT')
     console.log(String(process.env.GH_GRAPHQL_ENDPOINT))
     const client = new GraphQLClient(String(process.env.GH_GRAPHQL_ENDPOINT), {
       headers: {
