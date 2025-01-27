d3ff1d93f4ce5cb56a587b9ce36000a003ac86c8
Brenno Calado <brenno.calado@gmail.com>
2025-01-27 17:40:13 -0300

    fix: decrease contact font size

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,7 +7,7 @@ export function Contact() {
       <h2 className="text-2xl mt-4 mb-2">
         <FormattedMessage id="contact" />
       </h2>
-      <div className="flex flex-col text-7xl uppercase">
+      <div className="flex flex-col text-6xl uppercase">
         <Link className="hover:text-[#c5221f]" href="mailto:brenno.calado@gmail.com">Email</Link>
         <Link className="hover:text-[#0e76a8]" href="https://www.linkedin.com/in/brenno-calado/">Linkedin</Link>
         <Link className="hover:text-[#128c7e]" href="https://wa.me/5581992437721">Whatsapp</Link>
